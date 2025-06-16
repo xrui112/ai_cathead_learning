8af13005a94c559c1682081895f8d731f6639e0e
caathead <155051876@qq.com>
2025-06-16 17:08:32 +0800

    完成RAG测试

--- cathead-sy-app/src/main/java/cn/cathead/ai/config/OllamaConfig.java
+++ cathead-sy-app/src/main/java/cn/cathead/ai/config/OllamaConfig.java
@@ -5,14 +5,17 @@ import io.micrometer.observation.ObservationRegistry;
 import org.springframework.ai.model.tool.DefaultToolCallingManager;
 import org.springframework.ai.model.tool.ToolCallingManager;
 import org.springframework.ai.ollama.OllamaChatModel;
+import org.springframework.ai.ollama.OllamaEmbeddingModel;
 import org.springframework.ai.ollama.api.OllamaApi;
 import org.springframework.ai.ollama.api.OllamaOptions;
 import org.springframework.ai.ollama.management.ModelManagementOptions;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 public class OllamaConfig {
@@ -35,27 +38,36 @@ public class OllamaConfig {
         return new TokenTextSplitter();
     }
 
-//    @Bean
-//    public SimpleVectorStore vectorStore(@Value("${spring.ai.rag.embed}") String model, OllamaApi ollamaApi, OpenAiApi openAiApi) {
-//        if ("nomic-embed-text".equalsIgnoreCase(model)) {
-//            OllamaEmbeddingClient embeddingClient = new OllamaEmbeddingClient(ollamaApi);
-//            embeddingClient.withDefaultOptions(OllamaOptions.create().withModel("nomic-embed-text"));
-//            return new SimpleVectorStore(embeddingClient);
-//        } else {
-//            OpenAiEmbeddingClient embeddingClient = new OpenAiEmbeddingClient(openAiApi);
-//            return new SimpleVectorStore(embeddingClient);
-//        }
-//    }
-//
-//    @Bean
-//    public PgVectorStore pgVectorStore(@Value("${spring.ai.rag.embed}") String model, OllamaApi ollamaApi, OpenAiApi openAiApi, JdbcTemplate jdbcTemplate) {
-//        if ("nomic-embed-text".equalsIgnoreCase(model)) {
-//            OllamaEmbeddingClient embeddingClient = new OllamaEmbeddingClient(ollamaApi);
-//            embeddingClient.withDefaultOptions(OllamaOptions.create().withModel("nomic-embed-text"));
-//            return new PgVectorStore(jdbcTemplate, embeddingClient);
-//        } else {
-//            OpenAiEmbeddingClient embeddingClient = new OpenAiEmbeddingClient(openAiApi);
-//            return new PgVectorStore(jdbcTemplate, embeddingClient);
-//        }
-//    }
+    /**
+     * @param ollamaApi
+     * @return 文档信息缓存到内存
+     */
+    @Bean
+    public SimpleVectorStore vectorStore(OllamaApi ollamaApi) throws Exception {
+
+            OllamaEmbeddingModel embeddingModel = new OllamaEmbeddingModel(ollamaApi,
+                    OllamaOptions.builder().model("nomic-embed-text").build(),
+                    ObservationRegistry.NOOP,
+                    ModelManagementOptions.builder().build());
+
+            return SimpleVectorStore.builder(embeddingModel).build();
+
+    }
+
+    /**
+     *
+     * @param ollamaApi
+     * @return 文档信息缓存到内存
+     */
+    @Bean
+    public PgVectorStore pgVectorStore( OllamaApi ollamaApi, JdbcTemplate jdbcTemplate) throws Exception {
+
+            OllamaEmbeddingModel embeddingModel = new OllamaEmbeddingModel(ollamaApi,
+                    OllamaOptions.builder().model("nomic-embed-text").build(),
+                    ObservationRegistry.NOOP,
+                    ModelManagementOptions.builder().build());
+
+            return PgVectorStore.builder(jdbcTemplate,embeddingModel).build();
+
+    }
 }
